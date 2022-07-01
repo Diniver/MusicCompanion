a21c2db2518d9559f52a3ebecd92edc635c6c323
Diniver <bobo.music.star@gmail.com>
2022-07-01 17:21:03 +0100

    Braking stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatDialogModule} from '@angular/material/dialog';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
-  ],
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatDialogModule,
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
