96032670232118485c8d05c0474031b1915119a2
Diniver <bobo.music.star@gmail.com>
2022-07-18 13:38:02 +0100

    small fixes

--- src/app/services/audio.service.ts
+++ src/app/services/audio.service.ts
@@ -8,18 +8,15 @@ export class AudioService {
   constructor() {}
 
   playStop(btn: iButton) {
-    let player = <HTMLAudioElement>document.getElementById(btn.fileName);
+    let player = <HTMLAudioElement>document.getElementById(btn.btnID);
     player.volume = btn.volume / 100;
     player.loop = btn.loop;
-    console.log(player.volume);
     if (btn.isActive) {
       player.currentTime = btn.trimStart;
       player.play();
-      console.log(btn.btnTitle + ' is playing');
     } else {
       player.pause();
       player.currentTime = btn.trimStart;
-      console.log(btn.btnTitle + ' is paused/stopped');
     }
   }
 }
